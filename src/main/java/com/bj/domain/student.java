0681d826e3f925b63b20ba91fdfe5bf840c5020b
zhangyunpeng <zypi2016@icloud.com>
2020-12-10 11:20:29 +0800

    增加字段

--- src/main/java/com/bj/domain/student.java
+++ src/main/java/com/bj/domain/student.java
@@ -2,5 +2,29 @@ package com.bj.domain;
 
 public class student {
     String name;
+    Integer age;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
